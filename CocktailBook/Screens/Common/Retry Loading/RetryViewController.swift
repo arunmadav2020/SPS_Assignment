bf69f7134661a3ba6592219ecf326e860f5b0277
arunkumar_macmini <arun@applmaster.com>
2023-12-29 09:49:37 +0530

    feat: Handle retry Use localized string

---
+++ CocktailBook/Screens/Common/Retry Loading/RetryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RetryViewController.swift
+//  CocktailBook
+//
+//  Created by Arun Kumar on 28/12/2023.
+//
+
+import UIKit
+
+class RetryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
